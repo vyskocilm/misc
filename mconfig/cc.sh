b78c77f215eea7925c76b6d75af0987845b560aa
Michal Vyskocil <michal.vyskocil@gmail.com>
2014-12-18 13:46:06 +0100

    Problem: need to reproduce malamute-40
    Solution: decrease changing interval

--- mconfig/cc.sh
+++ mconfig/cc.sh
@@ -2,7 +2,7 @@
 set -x
 while true; do
     sed -i 's/value = 43/value = 42/' mconfig.conf
-    sleep 7
+    sleep 1
     sed -i 's/value = 42/value = 43/' mconfig.conf
-    sleep 11
+    sleep 1
 done
